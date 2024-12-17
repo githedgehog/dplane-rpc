d3ca0d0a309ad84c70197d3ddac42eeef793bc45
Fredi Raspall <fredi@githedgehog.com>
2024-12-17 17:49:06 +0100

    clib: Add some tests

---
+++ clib/test/test_buffer.c
@@ -0,0 +1,248 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "../src/wire.h"
+
+#define TEST() fprintf(stderr, "Running test '%s'...........\n", __FUNCTION__)
+
+/** BUFFER tests **/
+int test_buffer_available_data(void)
+{
+    TEST();
+
+    int r;
+    buffer_t *buff = buffer_new(2);
+    if (!buff)
+        return EXIT_FAILURE;
+
+    uint8_t val_u8;
+    uint16_t val_u16;
+    uint32_t val_u32;
+    uint64_t val_u64;
+
+    r = get_u8(buff, &val_u8);
+    assert(r == E_NOT_ENOUGH_DATA);
+
+    put_u8(buff, 1);
+
+    r = get_u16(buff, &val_u16);
+    assert(r == E_NOT_ENOUGH_DATA);
+
+    put_u8(buff, 2);
+    put_u8(buff, 3);
+
+    r = get_u32(buff, &val_u32);
+    assert(r == E_NOT_ENOUGH_DATA);
+
+    put_u8(buff, 4);
+    put_u8(buff, 5);
+
+    r = get_u64(buff, &val_u64);
+    assert(r == E_NOT_ENOUGH_DATA);
+
+
+    buffer_free(buff);
+    return EXIT_SUCCESS;
+}
+int test_buffer_conds(void)
+{
+    TEST();
+
+    int r;
+    buffer_t *buff = buffer_new(0);
+    if (!buff)
+        return EXIT_FAILURE;
+
+    /* Write octets one at a time and read them.
+     * Check that we read the value we wrote. We
+     * should not be able to write more than UINT16_MAX
+     * (64k), as the message length is bounded by that.
+     */
+    for(index_t n = 1; n <= UINT16_MAX; n++) {
+        uint8_t val_write = n;
+        uint8_t val_read = 0;
+        r = put_u8(buff, val_write);
+        if (r != E_OK) {
+            buffer_dump(buff);
+            assert(0);
+        }
+        assert(buff->w == n);
+        assert(get_u8(buff, &val_read) == E_OK);
+        assert(val_read == val_write);
+    }
+    assert(buff->w == UINT16_MAX);
+    assert(buff->capacity==UINT16_MAX);
+
+    /* next write attempt should fail */
+    assert(put_u8(buff, 0) == E_TOO_BIG);
+
+    buffer_free(buff);
+    return EXIT_SUCCESS;
+}
+int test_buffer_resize(void)
+{
+    TEST();
+    buffer_t *buff = buffer_new(1);
+    if (!buff)
+        return EXIT_FAILURE;
+
+    for(uint16_t i = 1; i <= 100; i++)
+        put_u8(buff, 0xFF);
+    assert(buff->capacity > 100);
+    assert(buff->w == 100);
+
+    buffer_dump(buff);
+
+    buffer_free(buff);
+    return EXIT_SUCCESS;
+}
+int test_buffer_write_utils(void)
+{
+    TEST();
+    buffer_t *buff = buffer_new(100);
+    if (!buff)
+        return EXIT_FAILURE;
+
+    put_u8(buff, 1);
+    put_u8(buff, 2);
+    put_u8(buff, 3);
+    put_u8(buff, 4);
+    assert(buff->w == 4);
+    buffer_dump(buff);
+
+    put_u16(buff, 0x5566);
+    assert(buff->w == 6);
+    buffer_dump(buff);
+
+    put_u32(buff, 0xaabbccdd);
+    assert(buff->w == 10);
+    buffer_dump(buff);
+
+    put_u64(buff, 0x00FF00FF00FF00FF);
+    assert(buff->w == 18);
+    buffer_dump(buff);
+
+    buffer_clear(buff);
+    buffer_dump(buff);
+    buffer_free(buff);
+    return EXIT_SUCCESS;
+}
+int test_buffer_read_utils(void)
+{
+    TEST();
+    buffer_t *b1 = buffer_new(0);
+    if (!b1)
+        return EXIT_FAILURE;
+
+    buffer_t *b2 = buffer_new(0);
+    if (!b2)
+        return EXIT_FAILURE;
+
+    /* write stuff to buffer b1 */
+    uint8_t n;
+    for (n = 1; n <= 4; n++)
+        put_u8(b1, n);
+    put_u16(b1, 0x5566);
+    put_u32(b1, 0xaabbccdd);
+    put_u64(b1, 0x00FF00FF00FF00FF);
+
+    /* read stuff from b1 */
+    int r;
+    uint16_t u16;
+    uint32_t u32;
+    uint64_t u64;
+
+    /* read the 4 u8's and store them in b2 */
+    for (n = 1; n <= 4; n++) {
+        uint8_t u8 = 0;
+        if ((r = get_u8(b1, &u8)) != 0)
+            return EXIT_FAILURE;
+        else
+            put_u8(b2, u8);
+    }
+    /* read the u16 and store in b2 */
+    if ((r = get_u16(b1, &u16)) != 0)
+        return EXIT_FAILURE;
+    else
+        put_u16(b2, u16);
+
+    /* read the u32 and store in b2 */
+    if ((r = get_u32(b1, &u32)) != 0)
+        return EXIT_FAILURE;
+    else
+        put_u32(b2, u32);
+
+    /* read the u64 and store in b2 */
+    if ((r = get_u64(b1, &u64)) != 0)
+        return EXIT_FAILURE;
+    else
+        put_u64(b2, u64);
+
+    /* the two buffers must be identical */
+    if (buffer_cmp(b1, b2) != 0) {
+        buffer_dump(b1);
+        buffer_dump(b2);
+        return EXIT_FAILURE;
+    }
+
+    buffer_free(b1);
+    buffer_free(b2);
+    return EXIT_SUCCESS;
+}
+int test_buffer_raw_read_write(void)
+{
+    TEST();
+
+    buffer_t *buff = buffer_new(0);
+    if (!buff)
+        return EXIT_FAILURE;
+
+    int r;
+    uint8_t some_data[25] = {0};
+    for(int i=0; i <sizeof(some_data); i++)
+        some_data[i] = i;
+
+    r = put_raw(buff, some_data, sizeof(some_data));
+    assert(r == E_OK);
+    assert(buff->w == sizeof(some_data));
+    buffer_dump(buff);
+
+    uint8_t recovered[25] = {0};
+    r = get_raw(buff, recovered, sizeof(recovered));
+    assert(r == E_OK);
+    assert(buff->r == sizeof(recovered));
+    buffer_dump(buff);
+
+    assert(memcmp(recovered, some_data, sizeof(recovered)) == 0);
+
+    buffer_free(buff);
+    return EXIT_SUCCESS;
+}
+
+
+/** ENCODE / DECODE **/
+
+
+int main(int argc, char **argv)
+{
+    if (test_buffer_conds() != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_buffer_available_data() != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_buffer_write_utils() != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_buffer_resize() != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_buffer_read_utils() != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_buffer_raw_read_write() != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    fprintf(stderr, "Success!\n");
+    return EXIT_SUCCESS;
+}
---
+++ clib/test/test_wire.c
@@ -0,0 +1,469 @@
+#include <string.h>
+#include <stdbool.h>
+
+#include "../src/wire.h"
+#include "../src/dp_msg.h"
+
+#define TEST() fprintf(stderr, "Running test '%s'...........\n", __FUNCTION__)
+#define CHECK(cond) do { if (!(cond)) {assert(0); return EXIT_FAILURE;}   }while(0)
+
+#define SIZE_SHOW(type) fprintf(stderr, "%32.32s: %zu\n", #type, sizeof(type));
+void dump_obj_sizes(void)
+{
+    fprintf(stderr, "MAX_ECMP: %u\n", MAX_ECMP);
+    fprintf(stderr, "MAX_NHOPS: %u\n", MAX_NHOPS);
+
+    SIZE_SHOW(struct ver_info);
+    SIZE_SHOW(struct rmac);
+    SIZE_SHOW(struct ifaddress);
+    SIZE_SHOW(struct ip_route);
+    SIZE_SHOW(struct next_hop);
+    SIZE_SHOW(struct RpcObject);
+    SIZE_SHOW(struct RpcRequest);
+    SIZE_SHOW(struct RpcResponse);
+    SIZE_SHOW(struct RpcControl);
+    SIZE_SHOW(struct RpcNotification);
+    SIZE_SHOW(struct RpcMsg);
+}
+void dump(void *s, size_t len, char *disp)
+{
+    uint8_t *octets = (uint8_t*)s;
+    fprintf(stderr, "%10.10s: ", disp);
+    for(int i=0; i < len; i++) {
+        fprintf(stderr, "%02x ", octets[i]);
+    }
+    fprintf(stderr, "\n");
+}
+void cmp(void *s1, void *s2, size_t len, bool show_all)
+{
+    uint8_t *octets1 = (uint8_t*)s1;
+    uint8_t *octets2 = (uint8_t*)s2;
+    for(int i=0; i < len; i++) {
+        if (show_all) {
+            fprintf(stderr, "(%d) %02x %02x %s\n", i, octets1[i], octets2[i], octets1[i] != octets2[i] ? "*" : "");
+        } else if (octets1[i] != octets2[i]) {
+            fprintf(stderr, "(%d) %02x %02x\n", i, octets1[i], octets2[i]);
+        }
+    }
+}
+
+int check_object(buffer_t *buff, struct RpcObject *object)
+{
+    int r;
+
+    /* encode the object */
+    r = encode_object(buff, object);
+    CHECK(r==E_OK);
+
+    /* decode into recovered */
+    struct RpcObject recovered;
+    memset(&recovered, 0, sizeof(recovered));
+
+    r= decode_object(buff, &recovered);
+    CHECK(r==E_OK);
+
+    CHECK(buff->r == buff->w);
+
+    /* compare recovered vs original */
+    r = memcmp(&recovered, object, sizeof(recovered));
+    if (r != 0) {
+        buffer_dump(buff);
+        dump(object, sizeof(struct RpcObject), "original");
+        dump(&recovered, sizeof(struct RpcObject), "recovered");
+        cmp(object, &recovered, sizeof(struct RpcObject), false);
+        assert(0);
+        return EXIT_FAILURE;
+    }
+    buffer_dump(buff);
+    return EXIT_SUCCESS;
+}
+int check_msg(buffer_t *buff, struct RpcMsg *msg)
+{
+    int r;
+
+    /* encode the object */
+    r = encode_msg(buff, msg);
+    CHECK(r==E_OK);
+
+    /* decode into recovered */
+    struct RpcMsg recovered;
+    memset(&recovered, 0, sizeof(recovered));
+
+    r = decode_msg(buff, &recovered);
+    CHECK(r==E_OK);
+    CHECK(buff->r == buff->w);
+
+    struct RpcObject *msg_objects = NULL;
+    struct RpcObject *rec_objects = NULL;
+
+    /* if msg has a response, it may point to a bunch of
+     * objects. Nullify pointers for the dummy comparison
+     * to work and later on compare the objects.
+     */
+    if (recovered.type == Response) {
+        msg_objects = msg->response.objects;
+        rec_objects = recovered.response.objects;
+        msg->response.objects = NULL;
+        recovered.response.objects = NULL;
+    }
+
+    /* compare recovered vs original */
+    r = memcmp(&recovered, msg, sizeof(recovered));
+    if (r != 0) {
+        buffer_dump(buff);
+        //dump(msg, sizeof(struct RpcMsg), "original");
+        //dump(&recovered, sizeof(struct RpcMsg), "recovered");
+        cmp(msg, &recovered, sizeof(struct RpcMsg), false);
+        return EXIT_FAILURE;
+    }
+
+    /* compare object array */
+    CHECK(!!rec_objects == !!rec_objects);
+    if (rec_objects) {
+        r = memcmp(msg_objects, rec_objects, sizeof(recovered));
+        if (r != 0) {
+            cmp(msg, &recovered, sizeof(struct RpcObject) * msg->response.num_objects, false);
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* decoded objects were allocated in heap */
+    recovered.response.objects = rec_objects;
+    msg_dispose(&recovered);
+
+    //buffer_dump(buff);
+    return EXIT_SUCCESS;
+}
+
+
+/* Build objects */
+struct rmac build_rmac(void)
+{
+    /* build rmac object */
+    struct rmac rmac = {0};
+    set_ip_address(&rmac.address, "7.0.0.9");
+    uint8_t mac[MAC_LEN] = {1,2,3,4,5,6};
+    set_mac_address(&rmac.mac, mac);
+    rmac.vni = 333;
+    return rmac;
+}
+struct ver_info build_ver_info(void)
+{
+    /* build verinfo object */
+    struct ver_info info = {.major = 1, .minor = 2, .patch = 3};
+    return info;
+}
+struct ifaddress build_ifaddress(void)
+{
+    struct ifaddress ifaddr = {0};
+    set_ip_address(&ifaddr.address, "10.0.0.1");
+    ifaddr.len = 30;
+    ifaddr.ifindex = 111;
+    ifaddr.vrfid = 999;
+    return ifaddr;
+}
+struct next_hop build_next_hop(const char *addr, Ifindex ifindex, Vni vni)
+{
+    struct next_hop nhop = {0};
+    nhop.ifindex = ifindex;
+    if (vni != 0) {
+        nhop.encap.type = VXLAN;
+        nhop.encap.vxlan.vni = vni;
+    }
+    set_ip_address(&nhop.address, addr);
+    return nhop;
+}
+struct ip_route build_ipv4_route(const char *prefix, NumNhops num_nhops)
+{
+    /* build route object */
+    struct ip_route route = {0};
+    int r = set_ip_address(&route.prefix, prefix);
+    route.len = 24;
+    route.vrfid = 1;
+    route.tableid = 44;
+    route.type = Bgp;
+    route.distance = 20;
+    route.metric = 100;
+    route.num_nhops = 0;
+    for (NumNhops i = 0; i < num_nhops; i++) {
+        struct next_hop nhop = build_next_hop("7.0.0.1", 700+i, 3000);
+            ip_route_add_nhop(&route, &nhop);
+    }
+    return route;
+}
+struct ip_route build_ipv6_route(const char *prefix, NumNhops num_nhops)
+{
+    /* build route object */
+    struct ip_route route = {0};
+    int r = set_ip_address(&route.prefix, prefix);
+    route.len = 64;
+    route.vrfid = 1;
+    route.tableid = 44;
+    route.type = Bgp;
+    route.distance = 20;
+    route.metric = 100;
+    route.num_nhops = 0;
+    for (NumNhops i = 0; i < num_nhops; i++) {
+        struct next_hop nhop = build_next_hop("2001:1:2:3::1", 700+i, 3000);
+            ip_route_add_nhop(&route, &nhop);
+    }
+    return route;
+}
+
+/* test object encoding / decoding */
+int test_object_rmac(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build rmac object */
+    struct rmac rmac = build_rmac();
+
+    /* wrap it */
+    struct RpcObject object = {.type = Rmac, .rmac = rmac};
+    return check_object(buff, &object);
+}
+int test_object_verinfo(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build verinfo object */
+    struct ver_info info = build_ver_info();
+
+    /* wrap it */
+    struct RpcObject object = {.type = VerInfo, .ver_info = info};
+    return check_object(buff, &object);
+}
+int test_object_ifaddr(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build ifaddress object */
+    struct ifaddress ifaddr = build_ifaddress();
+
+    /* wrap it */
+    struct RpcObject object = {.type = IfAddress, .ifaddress = ifaddr};
+    return check_object(buff, &object);
+}
+int test_object_iproute_v4(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build route object */
+    struct ip_route route = build_ipv4_route("192.168.1.0", 6);
+
+    /* wrap it */
+    struct RpcObject object;
+    memset(&object, 0, sizeof(object));
+    object.type = IpRoute;
+    object.route = route;
+    return check_object(buff, &object);
+}
+int test_object_iproute_v6(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build route object */
+    struct ip_route route = build_ipv6_route("2000:1:2:3:4::", 6);
+
+    /* wrap it */
+    struct RpcObject object;
+    memset(&object, 0, sizeof(object));
+    object.type = IpRoute;
+    object.route = route;
+    return check_object(buff, &object);
+}
+int test_object_encoding(buffer_t *buff)
+{
+    if (test_object_rmac(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_object_verinfo(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_object_ifaddr(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_object_iproute_v4(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_object_iproute_v6(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
+}
+
+/* test msg:Request encoding / decoding */
+int test_msg_request_connect(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build rmac */
+    struct ver_info info = build_ver_info();
+
+    /* build request with object */
+    struct RpcMsg msg = {.type = Request, .request.op = Connect, .request.seqn = 1234};
+    verinfo_as_object(&msg.request.object, &info);
+    return check_msg(buff, &msg);
+}
+int test_msg_request_rmac(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build rmac */
+    struct rmac rmac = build_rmac();
+
+    /* build request with object */
+    struct RpcMsg msg = {.type = Request, .request.op = Add, .request.seqn = 1234};
+    rmac_as_object(&msg.request.object, &rmac);
+    return check_msg(buff, &msg);
+}
+int test_msg_request_ifaddr(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build rmac */
+    struct ifaddress ifaddr = build_ifaddress();
+
+    /* build request with object */
+    struct RpcMsg msg = {.type = Request, .request.op = Del, .request.seqn = 1234};
+    ifaddress_as_object(&msg.request.object, &ifaddr);
+    return check_msg(buff, &msg);
+}
+int test_msg_request_ipv4_route(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build rmac */
+    struct ip_route route = build_ipv4_route("192.168.1.0", MAX_NHOPS);
+
+    /* build request with object */
+    struct RpcMsg msg = {.type = Request, .request.op = Update, .request.seqn = 1234};
+    iproute_as_object(&msg.request.object, &route);
+    return check_msg(buff, &msg);
+}
+int test_msg_request_ipv6_route(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    /* build rmac */
+    struct ip_route route = build_ipv6_route("2000:1:2:3:4::", MAX_NHOPS);
+
+    /* build request with object */
+    struct RpcMsg msg = {.type = Request, .request.op = Update, .request.seqn = 1234};
+    iproute_as_object(&msg.request.object, &route);
+    return check_msg(buff, &msg);
+}
+int test_msg_request(buffer_t *buff)
+{
+    if (test_msg_request_connect(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_msg_request_rmac(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_msg_request_ifaddr(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_msg_request_ipv4_route(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_msg_request_ipv6_route(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
+}
+
+/* test msg:Response encoding / decoding */
+int test_msg_response_without_objects(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    struct RpcMsg msg = {0};
+    struct RpcResponse resp = {0};
+
+    resp.op = Update;
+    resp.seqn = 654321;
+    resp.rescode = Ok;
+    resp.num_objects = 0;
+
+    msg.type = Response;
+    msg.response = resp;
+
+    return check_msg(buff, &msg);
+}
+int test_msg_response_with_objects(buffer_t *buff)
+{
+    TEST();
+    buffer_clear(buff);
+
+    int r;
+    struct RpcMsg msg;
+    memset(&msg, 0, sizeof(msg));
+    struct RpcResponse resp = {0};
+    struct RpcObject object_buffer[10] = {0};
+
+    resp.op = Update;
+    resp.seqn = 654321;
+    resp.rescode = Ok;
+    resp.objects = object_buffer;
+
+    for(int i = 0; i < 10; i++) {
+        struct ip_route route = build_ipv4_route("192.168.50.0", 3);
+        struct RpcObject object = {0};
+        iproute_as_object(&object, &route);
+        if (add_response_object(&resp, &object) != E_OK)
+            return EXIT_FAILURE;
+    }
+
+    msg.type = Response;
+    msg.response = resp;
+
+    return check_msg(buff, &msg);
+}
+int test_msg_response(buffer_t *buff)
+{
+    if (test_msg_response_without_objects(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    if (test_msg_response_with_objects(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
+}
+
+
+int main (int argc, char **argv)
+{
+    buffer_t *buff = buffer_new(0);
+    if (!buff)
+        return EXIT_FAILURE;
+
+    dump_obj_sizes();
+
+    /* test object encoding / decoding */
+    if (test_object_encoding(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    /* test msg:request encoding / decoding */
+    if (test_msg_request(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    /* test msg:response encoding / decoding */
+    if (test_msg_response(buff) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    buffer_free(buff);
+    fprintf(stderr, "Success!\n");
+    return EXIT_SUCCESS;
+}
