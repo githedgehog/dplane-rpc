0b827a6e21e8870770eb8b67449cf180b82be5e2
Fredi Raspall <fredi@githedgehog.com>
2024-12-17 14:23:25 +0100

    clib: Add simple buffer implementation and utils.
    A 'buffer_t' is the auxiliary data structure used to encode and
    decode information in the wire format.
    
    Since the wire protocol limits the max size of a message to 64KB,
    the library or users could use a fixed-sized 64K buffer (e.g.
    static or in stack). However, for flexibility, we allow buffer_t
    to grow as needed. In order to limit heap re-allocations, a
    buffer_t can be reutilized as many times as wanted, by calling
    buffer_clear(). So, we get the best of two worlds.

---
+++ clib/src/buffer.h
@@ -0,0 +1,28 @@
+#ifndef RPC_BUFFER_H_
+#define RPC_BUFFER_H_
+
+#include <stdint.h>
+
+#include "common.h"
+
+typedef uint32_t index_t;
+
+typedef struct buffer_s {
+    index_t w;
+    index_t r;
+    index_t capacity;
+    uint8_t *storage;
+} buffer_t;
+
+#define MAX(a,b) (a) < (b) ? (b) : (a)
+
+buffer_t *buffer_new(index_t capacity);
+void buffer_free(buffer_t *buff);
+void buffer_clear(buffer_t *buff);
+void buffer_dump(buffer_t *buff);
+int buffer_cmp(buffer_t *b1, buffer_t *b2);
+
+index_t buffer_get_roff(buffer_t *buff);
+index_t buffer_get_woff(buffer_t *buff);
+
+#endif /* RPC_BUFFER_H_ */
