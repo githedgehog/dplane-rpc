52f54603ba4809de4c22847e3b2bff949119341b
Fredi Raspall <fredi@githedgehog.com>
2024-12-17 14:30:59 +0100

    clib: Add utils to write/read from buffer_t's
    This C library tries to resemble the Rust one by implementing
    get/put functions to read / write data from / to buffers. This
    patch defines all the ones used so far to encode messages and
    objects.

---
+++ clib/src/wire.c
@@ -0,0 +1,49 @@
+#include <string.h>
+
+#include "wire.h"
+
+/* internal only */
+int buffer_enlarge(buffer_t *buff, size_t required);
+
+/* Buffer basic writers */
+DEF_PUT(u8, uint8_t);
+DEF_PUT(u16, uint16_t);
+DEF_PUT(u32, uint32_t);
+DEF_PUT(u64, uint64_t);
+
+/* Buffer basic readers */
+DEF_GET(u8, uint8_t);
+DEF_GET(u16, uint16_t);
+DEF_GET(u32, uint32_t);
+DEF_GET(u64, uint64_t);
+
+/* Buffer basic inserters */
+DEF_INSERT(u8, uint8_t);
+DEF_INSERT(u16, uint16_t);
+DEF_INSERT(u32, uint32_t);
+DEF_INSERT(u64, uint64_t);
+
+int put_raw(buffer_t *buff, void *data, size_t data_size)
+{
+    BUG(!buff || !data || !data_size, E_BUG);
+
+    if (buff->w + (index_t)data_size > buff->capacity) {
+        int r;
+        if ((r = buffer_enlarge(buff, data_size)) != E_OK)
+            return r;
+    }
+    memcpy(&buff->storage[buff->w], data, data_size);
+    buff->w += data_size;
+    return E_OK;
+}
+int get_raw(buffer_t *buff, void *data, size_t data_size)
+{
+    BUG(!buff || !data, E_BUG);
+
+    if (buff->r + (index_t)data_size > buff->w)
+        return E_NOT_ENOUGH_DATA;
+
+    memcpy(data, &buff->storage[buff->r], data_size);
+    buff->r += data_size;
+    return E_OK;
+}
