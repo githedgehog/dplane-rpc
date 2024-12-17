d83148e062c24510eef5f35acadb82b89756295a
Fredi Raspall <fredi@githedgehog.com>
2024-12-17 14:21:35 +0100

    clib: Add header with the types that objects are ..
    ... built from. This header includes the rust-exported proto.h.

---
+++ clib/src/dp_types.h
@@ -0,0 +1,30 @@
+#ifndef RPC_DP_TYPES_H_
+#define RPC_DP_TYPES_H_
+
+#include "proto.h"
+
+
+/**
+ * @brief Type to represent a MAC address.
+ *
+ * MAC addresses always consist of MAC_ADDR_LEN octets.
+ */
+struct mac_addr {
+    uint8_t bytes[MAC_LEN];
+};
+
+/**
+ * @brief Main type to represent an IP address.
+ *
+ * IP addresses appear in routes, interfaces, next-hops and IP-MAC bindings.
+ * If an address object should not contain an address, its ipver has to be set to NONE.
+ */
+struct ip_address {
+    IpVer ipver;
+    union {
+        uint32_t ipv4;
+        uint8_t ipv6[IPV6_ADDR_LEN];
+    }addr;
+};
+
+#endif /* RPC_DP_TYPES_H_ */
