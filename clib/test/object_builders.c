f5c7df4f89e0a7ccc50eae3504a2d76ffdeb0ef4
Fredi Raspall <fredi@githedgehog.com>
2024-12-19 17:48:54 +0100

    clib: Add object builders to object_builders.c|h
    Purpose: reusing these in other tests / binaries

---
+++ clib/test/object_builders.c
@@ -0,0 +1,77 @@
+#include "object_builders.h"
+
+/* Build objects */
+struct rmac build_rmac(void)
+{
+    /* build rmac object */
+    struct rmac rmac = {0};
+    set_ip_address(&rmac.address, "7.0.0.9");
+    uint8_t mac[MAC_LEN] = {1,2,3,4,5,6};
+    set_mac_address(&rmac.mac, mac);
+    rmac.vni = 333;
+    return rmac;
+}
+struct ver_info build_ver_info(void)
+{
+    /* build verinfo object */
+    struct ver_info info = {.major = 1, .minor = 2, .patch = 3};
+    return info;
+}
+struct ifaddress build_ifaddress(void)
+{
+    struct ifaddress ifaddr = {0};
+    set_ip_address(&ifaddr.address, "10.0.0.1");
+    ifaddr.len = 30;
+    ifaddr.ifindex = 111;
+    ifaddr.vrfid = 999;
+    return ifaddr;
+}
+struct next_hop build_next_hop(const char *addr, Ifindex ifindex, Vni vni)
+{
+    struct next_hop nhop = {0};
+    nhop.ifindex = ifindex;
+    if (vni != 0) {
+        nhop.encap.type = VXLAN;
+        nhop.encap.vxlan.vni = vni;
+    }
+    set_ip_address(&nhop.address, addr);
+    return nhop;
+}
+struct ip_route build_ipv4_route(const char *prefix, NumNhops num_nhops)
+{
+    /* build route object */
+    struct ip_route route = {0};
+    set_ip_address(&route.prefix, prefix);
+    route.len = 24;
+    route.vrfid = 1;
+    route.tableid = 44;
+    route.type = Bgp;
+    route.distance = 20;
+    route.metric = 100;
+    route.num_nhops = 0;
+    for (NumNhops i = 0; i < num_nhops; i++) {
+        struct next_hop nhop = build_next_hop("7.0.0.1", 700+i, 3000);
+            ip_route_add_nhop(&route, &nhop);
+    }
+    return route;
+}
+struct ip_route build_ipv6_route(const char *prefix, NumNhops num_nhops)
+{
+    /* build route object */
+    struct ip_route route = {0};
+    set_ip_address(&route.prefix, prefix);
+    route.len = 64;
+    route.vrfid = 1;
+    route.tableid = 44;
+    route.type = Bgp;
+    route.distance = 20;
+    route.metric = 100;
+    route.num_nhops = 0;
+    for (NumNhops i = 0; i < num_nhops; i++) {
+        struct next_hop nhop = build_next_hop("2001:1:2:3::1", 700+i, 3000);
+            ip_route_add_nhop(&route, &nhop);
+    }
+    return route;
+}
+
+
