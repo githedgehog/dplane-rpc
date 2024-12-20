0261ec8bc15aa0f9b5df00f505a75bcf08c641a4
Fredi Raspall <fredi@githedgehog.com>
2024-12-20 10:26:13 +0100

    clib: Add some stringifiers

---
+++ clib/src/display.h
@@ -0,0 +1,10 @@
+#ifndef SRC_DISPLAY_H_
+#define SRC_DISPLAY_H_
+
+#include "dp_msg.h"
+
+const char *str_msg_type(MsgType type);
+const char *str_object_type(ObjType type);
+const char *str_rpc_op(RpcOp op);
+
+#endif /* SRC_DISPLAY_H_ */
