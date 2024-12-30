a4b247a897c166c4043db79d09241a692e7d46fb
Fredi Raspall <fredi@githedgehog.com>
2024-12-30 15:55:24 +0100

    clib: add header dplane-rpc.h
    The header includes all other headers (nothing is really private).
    Cmake script is updated to install all headers in /usr/include/
    dplane-rpc. The dynamic library is installed in /usr/local/lib.

---
+++ clib/src/dplane-rpc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "common.h"
+#include "buffer.h"
+#include "vec.h"
+#include "wire.h"
+#include "display.h"
+#include "dp_objects.h"
+#include "dp_msg.h"
+#include "proto.h"
