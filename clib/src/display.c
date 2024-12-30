300c9147c2b3542309b5850e3bbe295745cecd5e
Fredi Raspall <fredi@githedgehog.com>
2024-12-30 22:00:28 +0100

    clib: move stringifier to display.c|h

--- clib/src/display.c
+++ clib/src/display.c
@@ -1,4 +1,5 @@
 #include "display.h"
+#include "common.h"
 
 /* Stringify message type */
 const char *str_msg_type(MsgType type)
@@ -71,5 +72,42 @@ const char *str_rescode(RpcResultCode code)
         return "ExpectMore";
     default:
         return "Unknown";
+    }
+}
+
+/* stringify library return codes */
+const char *err2str(int e)
+{
+    switch (e) {
+    case E_OK:
+        return "Ok";
+    case E_BUG:
+        return "Bug";
+    case E_OOM:
+        return "Out-of-memory";
+    case E_NOT_ENOUGH_DATA:
+        return "Not-enough-data";
+    case E_TOO_BIG:
+        return "Msg-too-big";
+    case E_INVAL:
+        return "Invalid argument";
+    case E_INVALID_DATA:
+        return "Invalid data";
+    case E_INCONSIST_LEN:
+        return "Msg-inconsistent-length";
+    case E_INVALID_MSG_TYPE:
+        return "Invalid msg type";
+    case E_EXCESS_BYTES:
+        return "Excess data";
+    case E_TOO_MANY_NHOPS:
+        return "Too many next-hops";
+    case E_TOO_MANY_OBJECTS:
+        return "Too many objects";
+    case E_TOO_MANY_MATCH_VALUES:
+        return "Too many match values";
+    case E_VEC_CAPACITY_EXCEEDED:
+        return "Vector capacity exceeded";
+    default:
+        return "Other/Unknown";
     }
 }
--- clib/src/display.h
+++ clib/src/display.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "dp_msg.h"
+#include "proto.h"
 
 const char *str_msg_type(MsgType type);
 const char *str_object_type(ObjType type);
 const char *str_rpc_op(RpcOp op);
 const char *str_rescode(RpcResultCode code);
+const char *err2str(int e);
