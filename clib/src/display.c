3a921a1bc73f20b8342198afd14c08eec7b41332
Fredi Raspall <fredi@githedgehog.com>
2025-01-12 16:48:22 +0100

    clib: Add object formatters
    Add object formatters to easily dump objects in logs.

--- clib/src/display.c
+++ clib/src/display.c
@@ -1,5 +1,10 @@
+#include <arpa/inet.h>
+
+#include "common.h"
 #include "display.h"
+#include "dp_objects.h"
 #include "errors.h"
+#include "fmt_buff.h"
 
 /* Stringify message type */
 const char *str_msg_type(MsgType type)
@@ -111,5 +116,189 @@ const char *err2str(int e)
         return "String is too long";
     default:
         return "Other/Unknown";
+    }
+}
+
+/* stringify iproute type/protocol */
+const char *route_type_str(RouteType rt)
+{
+    switch (rt) {
+    case Connected:
+        return "connected";
+    case Static:
+        return "static";
+    case Ospf:
+        return "OSPF";
+    case Isis:
+        return "IS-IS";
+    case Bgp:
+        return "BGP";
+    case Other:
+        return "Other";
+    default:
+        return "Unknown";
+    }
+}
+
+/* basic type formatters */
+char *fmt_mac(struct fmt_buff *fb, bool clear, const char *prefix, struct mac_addr *mac)
+{
+    BUG(!fb || !mac, NULL);
+    if (clear)
+        clear_fmt_buff(fb);
+
+    if (prefix)
+        fmt_buff(fb, "%s", prefix);
+
+    return fmt_buff(fb, "%02x:%02x:%02x:%02x:%02x:%02x",
+                    mac->bytes[0], mac->bytes[1],
+                    mac->bytes[2], mac->bytes[3],
+                    mac->bytes[4], mac->bytes[5]);
+}
+char *fmt_ipaddr(struct fmt_buff *fb, bool clear, const char *disp_prefix, struct ip_address *ip)
+{
+    BUG(!fb || !ip, NULL);
+    char b[INET_ADDRSTRLEN];
+    if (clear)
+        clear_fmt_buff(fb);
+
+    if (disp_prefix)
+        fmt_buff(fb, "%s", disp_prefix);
+
+    switch (ip->ipver) {
+    case NONE:
+        return fmt_buff(fb, "none");
+    case IPV4:
+        return fmt_buff(fb, "%s", inet_ntop(AF_INET, &ip->addr, b, sizeof(b)));
+    case IPV6:
+        return fmt_buff(fb, "%s", inet_ntop(AF_INET6, &ip->addr, b, sizeof(b)));
+    default:
+        return fmt_buff(fb, "unknown IP version '%u'", ip->ipver);
+    }
+}
+char *fmt_prefix(struct fmt_buff *fb, bool clear, const char *disp_prefix, struct ip_address *ip, uint8_t pref_len)
+{
+    BUG(!fb || !ip, NULL);
+    char b[INET_ADDRSTRLEN];
+    if (clear)
+        clear_fmt_buff(fb);
+
+    if (disp_prefix)
+        fmt_buff(fb, "%s", disp_prefix);
+
+    switch (ip->ipver) {
+    case NONE:
+        return fmt_buff(fb, "none");
+    case IPV4:
+        return fmt_buff(fb, "%s/%u", inet_ntop(AF_INET, &ip->addr, b, sizeof(b)), pref_len);
+    case IPV6:
+        return fmt_buff(fb, "%s/%u", inet_ntop(AF_INET6, &ip->addr, b, sizeof(b)), pref_len);
+    default:
+        return fmt_buff(fb, "unknown IP version '%u'", ip->ipver);
+    }
+}
+
+/* object formatters */
+char *fmt_verinfo(struct fmt_buff *fb, bool clear, struct ver_info *v)
+{
+    BUG(!fb || !v, NULL);
+    if (clear)
+        clear_fmt_buff(fb);
+    return fmt_buff(fb, "verinfo ─── v%u.%u.%u", v->major, v->minor, v->patch);
+}
+char *fmt_rmac(struct fmt_buff *fb, bool clear, struct rmac *rmac)
+{
+    BUG(!fb || !rmac, NULL);
+    if (clear)
+        clear_fmt_buff(fb);
+
+    fmt_buff(fb, "rmac ─── vni: %u", rmac->vni);
+    fmt_ipaddr(fb, false, " ip:", &rmac->address);
+    return fmt_mac(fb, false, " mac:", &rmac->mac);
+}
+char *fmt_ifaddress(struct fmt_buff *fb, bool clear, struct ifaddress *ifaddr)
+{
+    BUG(!fb || !ifaddr, NULL);
+    if (clear)
+        clear_fmt_buff(fb);
+    fmt_buff(fb, "Ifaddress ─── ifname: %s", ifaddr->ifname);
+    fmt_prefix(fb, false, " ip:", &ifaddr->address, ifaddr->len);
+    return fmt_buff(fb, " ifindex:%u vrfid:%u", ifaddr->ifindex, ifaddr->vrfid);
+}
+char *fmt_iproute(struct fmt_buff *fb, bool clear, struct ip_route *route)
+{
+    BUG(!fb || !route, NULL);
+    if (clear)
+        clear_fmt_buff(fb);
+    fmt_buff(fb, "Iproute ─── vrfid:%u tbl:%u", route->vrfid, route->tableid);
+    fmt_buff(fb, " %s [%u/%u]", route_type_str(route->type), route->distance, route->metric);
+    fmt_prefix(fb, false, " ", &route->prefix, route->len);
+
+    struct next_hop *nhop;
+    for (unsigned int i = 0; i < route->num_nhops; i++) {
+        nhop = &route->nhops[i];
+        fmt_buff(fb, " via");
+        if (nhop->address.ipver == IPV4 || nhop->address.ipver == IPV6)
+            fmt_ipaddr(fb, false, " ", &nhop->address);
+        if (nhop->ifindex)
+            fmt_buff(fb, " ifindex:%u", nhop->ifindex);
+        fmt_buff(fb, " vrfid:%u", nhop->vrfid);
+
+        switch (nhop->encap.type) {
+        case NoEncap:
+            fmt_buff(fb, " encap:None");
+            break;
+        case VXLAN:
+            fmt_buff(fb, " encap: VxLAN(vni:%u)", nhop->encap.vxlan.vni);
+            break;
+        default:
+            fmt_buff(fb, " encap: unknown type (%u)", nhop->encap.type);
+            break;
+        }
+    }
+    return fb->buff;
+}
+char *fmt_getfilter(struct fmt_buff *fb, bool clear, struct get_filter *filter)
+{
+    BUG(!fb || !filter, NULL);
+    if (clear)
+        clear_fmt_buff(fb);
+
+    fmt_buff(fb, "Getfilter ─── otypes(%zu):", filter->otypes.len);
+    for (size_t i = 0; i < filter->otypes.len; i++) {
+        uint8_t otype = filter->otypes.data[i];
+        fmt_buff(fb, " %s", str_object_type(otype));
+    }
+
+    fmt_buff(fb, " vrfIds(%zu):", filter->vrfIds.len);
+    for (size_t i = 0; i < filter->vrfIds.len; i++) {
+        uint32_t vrfid = filter->vrfIds.data[i];
+        fmt_buff(fb, " %u", vrfid);
+    }
+    return fb->buff;
+}
+char *fmt_rpcobject(struct fmt_buff *fb, bool clear, struct RpcObject *object)
+{
+    BUG(!fb || !object, NULL);
+
+    switch (object->type) {
+    case None:
+        if (clear)
+            clear_fmt_buff(fb);
+        return fmt_buff(fb, "none");
+    case VerInfo:
+        return fmt_verinfo(fb, clear, &object->ver_info);
+    case IfAddress:
+        return fmt_ifaddress(fb, clear, &object->ifaddress);
+    case Rmac:
+        return fmt_rmac(fb, clear, &object->rmac);
+    case IpRoute:
+        return fmt_iproute(fb, clear, &object->route);
+    case GetFilter:
+        return fmt_getfilter(fb, clear, &object->get_filter);
+    default:
+        if (clear)
+            clear_fmt_buff(fb);
+        return fmt_buff(fb, "unknown object type '%u'", object->type);
     }
 }
--- clib/src/display.h
+++ clib/src/display.h
@@ -1,9 +1,27 @@
 #pragma once
 
+#include "dp_objects.h"
+#include "fmt_buff.h"
 #include "proto.h"
+#include <stdbool.h>
 
+/* stringifiers */
 const char *str_msg_type(MsgType type);
 const char *str_object_type(ObjType type);
 const char *str_rpc_op(RpcOp op);
 const char *str_rescode(RpcResultCode code);
 const char *err2str(int e);
+const char *route_type_str(RouteType rt);
+
+/* custom type formatters */
+char *fmt_mac(struct fmt_buff *fb, bool clear, const char *prefix, struct mac_addr *mac);
+char *fmt_ipaddr(struct fmt_buff *fb, bool clear, const char *disp_prefix, struct ip_address *ip);
+char *fmt_prefix(struct fmt_buff *fb, bool clear, const char *disp_prefix, struct ip_address *ip, uint8_t pref_len);
+
+/* object formatters */
+char *fmt_verinfo(struct fmt_buff *fb, bool clear, struct ver_info *vinfo);
+char *fmt_rmac(struct fmt_buff *fb, bool clear, struct rmac *rmac);
+char *fmt_ifaddress(struct fmt_buff *fb, bool clear, struct ifaddress *ifaddr);
+char *fmt_iproute(struct fmt_buff *fb, bool clear, struct ip_route *route);
+char *fmt_getfilter(struct fmt_buff *fb, bool clear, struct get_filter *filter);
+char *fmt_rpcobject(struct fmt_buff *fb, bool clear, struct RpcObject *object);
